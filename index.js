dcb50e876345f2e96a38d87dd65e6125c3348fc0
Rakshit Dubey <119107044+RakshitDubey@users.noreply.github.com>
2024-07-30 01:28:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-board-frontend-269n.vercel.app',
+    origin:'https://job-board-frontend-mocha.vercel.app',
     credentials:true
 }
 
