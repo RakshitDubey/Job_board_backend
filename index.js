ab6a10e230467f40fb68bfa13ecc2f08feded6b3
Rakshit Dubey <119107044+RakshitDubey@users.noreply.github.com>
2024-07-30 00:12:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+app.get('/hello',(req,res)=>{
+    res.send("hello")
+})
+    
 
 
 
